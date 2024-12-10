2db91d0ba8499a4c36ea9f41068392186a281a55
Lee jung woon <149851043+MotherBoardHal9000@users.noreply.github.com>
2024-12-10 10:16:24 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let tyPographH2 = document.querySelector('.sectionIContents h2');
+let popIt = document.getElementById('popIt');
 let sPonge = document.getElementById('sPonge');
 let aBstract = document.getElementById('aBstract');
 let Brick = document.getElementById('brick');
@@ -9,8 +10,9 @@ let piPe = document.getElementById("piPe");
 
 window.addEventListener("scroll",function(addEventListener){
     let value = window.scrollY;
-    piPe.style.transform = `translateY(${value * 0.85}px)`
+    piPe.style.transform = `translateY(${value * 0.47}px)`
     aBstract.style.transform = `translateY(${value * 0.45}px)`
+    popIt.style.transform=`translateY(${value * 0.40}px)`
     sPonge.style.transform = `translateY(${value * 0.25}px)`
     Brick.style.transform = `translateY(${value * 0.15}px)`
     Fluid.style.transform = `translateY(${value * 0.25}px)`
